06b9cc31c2cbf7ce7a6bcc17404ca18a6ebf7fbe
Vladislav Chesnov <chesnov@medods.ru>
2022-12-24 06:10:55 +0500

    release 0.11.3

--- src/directive.js
+++ src/directive.js
@@ -45,7 +45,7 @@ export default function (el, binding) {
     var position = el.selectionEnd
     // save the character just inserted
     var digit = el.value[position-1]
-    el.value = masker(el.value, config.mask, true, config.tokens)
+    el.value = masker(el.value, (config || {}).mask, true, config.tokens)
     // if the digit was changed, increment position until find the digit again
     while (position < el.value.length && el.value.charAt(position-1) !== digit) {
       position++
